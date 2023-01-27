b2494a405cfa2582cb65f23a06beb509b8941aff
Dzaky Rafi <dzakylmg87@gmail.com>
2023-01-27 11:27:34 +0700

    Add For deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,20 @@
 /** @type {import('@sveltejs/kit').Config} */
+import adapter from '@sveltejs/adapter-netlify';
+
 const config = {
 	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
+		// default options are shown
+		adapter: adapter({
+		  // if true, will create a Netlify Edge Function rather
+		  // than using standard Node-based functions
+		  edge: false,
+	 
+		  // if true, will split your app into multiple functions
+		  // instead of creating a single one for the entire app.
+		  // if `edge` is true, this option cannot be used
+		  split: false
+		})
+	  }
 };
 
 export default config;
